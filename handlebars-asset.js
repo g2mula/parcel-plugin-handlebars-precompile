7fae6479d90c294e7180999fd710b6604374ccaf
Mula Njira <g2mula@gmail.com>
2021-03-24 20:09:52 +0300

    Register partial.

--- handlebars-asset.js
+++ handlebars-asset.js
@@ -6,6 +6,7 @@ class HbsAsset extends JSAsset {
         const precompiled = Handlebars.precompile(this.contents);
         this.contents = `
                 import Handlebars from 'handlebars/dist/handlebars.runtime';
+                Handlebars.registerPartial('${this.name}', ${precompiled});
                 const templateFunction = Handlebars.template(${precompiled});
                 export default templateFunction;
                 `;
